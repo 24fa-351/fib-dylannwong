276902513b67d7edcd902ce8f5da8a1d41687012
dyl <dylanwong12312@gmail.com>
2024-09-05 21:37:17 -0700

    working fibonacci program

--- fib.c
+++ fib.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello!\n");
+int fibbi(int n) {
+
+      int num1 = 0;
+      int num2 = 1;
+      int sum = 1;
+
+   if(n == 1) {
+      return 0;
+   } else {
+      for(int i = 0; i < n - 1; ++i) {
+         if(i + 2 == n) {
+            return sum;
+         }
+         sum = num1 + num2;
+         num1 = num2;
+         num2 = sum;
+      }
+   }
+   
+}
+
+int fibbr(int n) {
+
+   if(n == -1) {
+      return 0;
+   }
+   if (n == 0 || n == 1) {
+      return 1;
+   }
+
+   return fibbr(n - 1) + fibbr(n - 2);
+}
+
+int main(int argc, char *argv[]) {
+
+   FILE *fptr = fopen(argv[3], "r");
+      char str1[100];
+      fgets(str1, 100, fptr);
+      int input1 = atoi(str1);
+      int input2 = atoi(argv[1]);
+      int n = input1 + input2;
+
+   if(*argv[2]=='i') {
+      printf("%d", fibbi(n));
+   } else if(*argv[2]=='r') {
+      printf("%d", fibbr(n-2));
+   }
+   
    return 0;
 }
+
+
